e92831e5e40e681dbe74c4055d76899dd5080403
Rui Dantas <ruidantas55@gmail.com>
2023-01-19 23:45:12 +0000

    Write INSERT queries to add data to all of the tables.

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password)
+VALUES ('Rui Dantas', 'ruidantas55@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Courtney Metzger', 'cm1998@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Janky Jank', 'jank@gmail.com', 'janky');
+
+INSERT INTO properties (owner_id, title, description, parking_spaces, cost_per_night, num_of_bathrooms, num_of_bedrooms, thumbnail_photo_url, cover_photo_url, country, street, city, province, post_code, active)
+
+VALUES (1, 'Speed lamp', 'description', 5, 'img', 'img',    930, 6, 4,   8, 'Canada' , '536 Namsub Highway', 'Sotboske, Quebec'  , '28142' ,true);
